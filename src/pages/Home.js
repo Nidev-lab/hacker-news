75f83ae526b522779a92fd04dee6bfe280bd63ad
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-01-18 00:06:13 -0300

    dropdown added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import './home.css'
-import React from 'react'
+import './home.css';
+import React from 'react';
 import Card from '../components/Card/Card';
 import Header from '../components/Header/Header';
 
@@ -8,6 +8,17 @@ const Home = () => {
     <>
       <Header />
       <main className='container'>
+        <select name="cars" id="cars" className='dropdown-technologies col-4'>
+          <option value="volvo">
+            Angular
+          </option>
+          <option value="saab">
+              React
+          </option>
+          <option value="mercedes">
+            Vue
+          </option>
+        </select>
         <Card />
       </main>
     </>
