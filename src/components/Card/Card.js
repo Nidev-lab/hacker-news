9a0291bef125535f84fcec1c2df009f59b110e62
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-01-18 23:45:27 -0300

    Consume API and card with data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,32 +3,24 @@ import React from 'react';
 import clock from '../../assets/icons/clock.svg';
 import favempty from '../../assets/icons/favempty.svg'
 
-const Card = () => {
+const Card = ({ data }) => {
+  if (!data.created_at || !data.author || !data.story_url || !data.story_title){
+    return null
+  }
+
   return (
-    <div className='container-cards'>
-      <div className='card col-12 col-md-6 col-lg-6'>
-        <div className='info-box'>
-          <div className='position-time-news'>
-            <img src={clock} alt="clock-card" />
-            <p className='font-time-news'>3 hours ago by author</p>
-          </div>
-          <p className='font-description-news'>Yes, React is taking over front-end development. The question is why.</p>
+    <div className='card col-12 col-md-6 col-lg-6'>
+      <div className='info-box'>
+        <div className='position-time-news'>
+          <img src={clock} alt="clock-card" />
+          <p className='font-time-news'>{data.created_at} {data.author}</p>
         </div>
-        <div className='favorite-box'>
-          <img src={favempty} alt="" />
-        </div>
+        <a href={data.story_url}>
+          <p className='font-description-news'>{data.story_title}</p>
+        </a>
       </div>
-      <div className='card col-12 col-md-6 col-lg-6'>
-        <div className='info-box'>
-          <div className='position-time-news'>
-            <img src={clock} alt="clock-card" />
-            <p className='font-time-news'>3 hours ago by author</p>
-          </div>
-          <p className='font-description-news'>Yes, React is taking over front-end development. The question is why.</p>
-        </div>
-        <div className='favorite-box'>
-          <img src={favempty} alt="" />
-        </div>
+      <div className='favorite-box'>
+        <img src={favempty} alt="" />
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,38 @@
 import './home.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card/Card';
 import Header from '../components/Header/Header';
 
 const Home = () => {
+  
+  const [hits, setHits] = useState([])
+
+  const handleChange = (e) => {
+    fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${e.target.value}&page=0`)
+      .then(resp => resp.json())
+      .then(json => setHits(json.hits))
+  }
+
   return (
     <>
       <Header />
       <main className='container'>
-        <select name="cars" id="cars" className='dropdown-technologies col-4'>
-          <option value="volvo">
+        <select name="cars" id="cars" className='dropdown-technologies col-4' onChange={handleChange}>
+          <option value="angular">
             Angular
           </option>
-          <option value="saab">
-              React
+          <option value="reactjs">
+            React
           </option>
-          <option value="mercedes">
+          <option value="vuejs">
             Vue
           </option>
         </select>
-        <Card />
+        <div className='container-cards'>
+          {
+            hits.map(data => <Card data={data}/>)
+          }
+        </div>
       </main>
     </>
   )
