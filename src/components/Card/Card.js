fba26745d9ec280e894aeb233c520a04b2bf9e9c
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-01-19 20:30:01 -0300

    Fix/refactor home (#13)
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,35 +6,41 @@ import favfull from '../../assets/icons/favfull.svg'
 
 const Card = ({ data, favorite }) => {
 
-  if (!data.created_at || !data.author || !data.story_url || !data.story_title){
-    return null
-  }
-
-  const handleFavClick = data => {
+  const handleFavClick = info => {
     let existingFavs = JSON.parse(localStorage.getItem("myFavs")) || []
     let newFavs
 
-    existingFavs.push(data)
+    existingFavs.push(info)
     newFavs = existingFavs
 
     localStorage.setItem("myFavs", JSON.stringify(newFavs))
   }
 
-
   return (
-    <div className='card col-12 col-md-6 col-lg-6'>
-      <a href={data.story_url} target='_blank' rel="noreferrer">
-        <div className='info-box'>
-          <div className='position-time-news'>
-            <img src={clock} alt="clock-card" />
-            <p className='font-time-news'>{data.created_at} {data.author}</p>
-          </div>
-            <p className='font-description-news'>{data.story_title}</p>
-        </div>
-      </a>
-      <div className='favorite-box'>
-        <img src={favorite ? favfull : favempty} alt="fav-icon" onClick={() => handleFavClick(data)}/>
-      </div>
+    <div className='container-cards'>
+      {
+        data.map((info, i) => {
+          if (!info.story_url || !info.created_at || !info.author || !info.story_title) {
+            return null
+          }
+          return (
+            <div className='card col-12 col-md-6 col-lg-6' key={i}>
+              <a href={info.story_url} target='_blank' rel="noreferrer">
+                <div className='info-box'>
+                  <div className='position-time-news'>
+                    <img src={clock} alt="clock-card" />
+                    <p className='font-time-news'>{info.created_at} {info.author}</p>
+                  </div>
+                  <p className='font-description-news'>{info.story_title}</p>
+                </div>
+              </a>
+              <div className='favorite-box'>
+                <img src={favorite.some(({ objectID }) => objectID === info.objectID) ? favfull : favempty} alt="fav-icon" onClick={() => handleFavClick(info)} />
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
