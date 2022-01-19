5dd39979437a459f21b6ee121089bc48a6ef96c7
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-01-19 10:51:02 -0300

    Fix some console errors

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -5,7 +5,7 @@ const Pagination = ({ pageQuantity, setPage }) => {
   return (
     <div className='position-pagination'>
       {[...Array(pageQuantity)].map((x, i) =>
-        <button className='primary-button' onClick={() => setPage(i)}>{i}</button>
+        <button className='primary-button' onClick={() => setPage(i)} key={i}>{i}</button>
       )}
     </div>
   )
