9a3d66864024ff9359ee2fc5554f30b889734073
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-01-19 02:52:57 -0300

    Logic and layout added (#6)
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,32 @@
 import './home.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card/Card';
 import Header from '../components/Header/Header';
+import Pagination from '../components/Pagination/Pagination';
 
 const Home = () => {
   
+  // const filter = localStorage.getItem("filter") || 'angular'
   const [hits, setHits] = useState([])
+  const [filter, setFilter] = useState('angular')
+  const [page, setPage] = useState(0)
+  const [pageQuantity, setpageQuantity] = useState(0)
 
   const handleChange = (e) => {
-    fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${e.target.value}&page=0`)
-      .then(resp => resp.json())
-      .then(json => setHits(json.hits))
+    // console.log(e.target.value)
+    // localStorage.setItem("filter", e.target.value)
+    setFilter(e.target.value)
   }
+
+  useEffect(() => {
+    fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${filter}&page=${page}`)
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json)
+        setHits(json.hits)
+        setpageQuantity(json.nbPages)
+      })
+  }, [filter, page]);
 
   return (
     <>
@@ -33,6 +48,7 @@ const Home = () => {
             hits.map(data => <Card data={data}/>)
           }
         </div>
+        <Pagination pageQuantity={pageQuantity} setPage={setPage}/>
       </main>
     </>
   )
