91017e002cbea80042a3ecb7b98a7e34c592c1fc
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-01-20 16:52:40 -0300

    Update (#24)
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -36,17 +36,21 @@ const Card = ({ data }) => {
           }
           return (
             <div className='card col-12 col-md-6 col-lg-6' key={i}>
-              <a href={info.story_url} target='_blank' rel="noreferrer">
-                <div className='info-box'>
-                  <div className='position-time-news'>
-                    <img src={clock} alt="clock-card" />
-                    <p className='font-time-news'>{info.created_at} {info.author}</p>
+              <div>
+                <a href={info.story_url} target='_blank' rel="noreferrer">
+                  <div className='info-box'>
+                    <div className='position-time-news'>
+                      <img src={clock} alt="clock-card" />
+                      <p className='font-time-news'>{info.created_at} {info.author}</p>
+                    </div>
+                    <p className='font-description-news' data-testid="data-title">{info.story_title}</p>
                   </div>
-                  <p className='font-description-news' data-testid="data-title">{info.story_title}</p>
+                </a>
+              </div>
+              <div>
+                <div className='favorite-box'>
+                  <img src={favorite.some(({ objectID }) => objectID === info.objectID) ? favfull : favempty} alt="fav-icon" onClick={() => handleFavClick(info)} className='fav-icon-style'/>
                 </div>
-              </a>
-              <div className='favorite-box'>
-                <img src={favorite.some(({ objectID }) => objectID === info.objectID) ? favfull : favempty} alt="fav-icon" onClick={() => handleFavClick(info)} className='fav-icon-style'/>
               </div>
             </div>
           )
