0c0f921b55576c04c544ede7c122558f93f44838
Nico Gonzalez <53904695+Nidev-lab@users.noreply.github.com>
2022-01-20 02:32:12 -0300

    Fix/pagination styles (#15)
    * Test with jest added
    Co-authored-by: Nicolas Gonzalez <ngonzalez@dminc.com>

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,16 +1,22 @@
 import './pagination.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 const Pagination = ({ pageQuantity, setPage }) => {
+  const [currentPage, setCurrentPage] = useState(1)
+
   if (!pageQuantity) {
     return null
   }
 
   return (
-    <div className='position-pagination' data-testid="pagination-test">
-      {[...Array(pageQuantity)].map((x, i) =>
-        <button className='primary-button' onClick={() => setPage(i)} key={i}>{i}</button>
-      )}
+    <div className='position-pagination'>
+      <a className='primary-button' href={`#${currentPage}`} onClick={() => setCurrentPage(currentPage - 11 > 1 ?currentPage - 11 : 1)}>{'<'}</a>
+      <div className='style-pagination' data-testid="pagination-test">
+        {[...Array(pageQuantity)].map((x, i) =>
+          <button className='primary-button' onClick={() => setPage(i)} key={i} id={i+1}>{i+1}</button>
+        )}
+      </div>
+      <a className='primary-button' href={`#${currentPage}`} onClick={() => setCurrentPage(currentPage + 11 < pageQuantity ? currentPage + 11 : pageQuantity )}>{'>'}</a>
     </div>
   )
 };
